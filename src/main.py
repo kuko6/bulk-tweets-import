ef23b5dc28ca82d6acc5cb24aeaf8a6e9b2bdd5a
Jakub Povinec <jakub.povinec@gmail.com>
2022-10-03 10:51:02 +0200

    comments

--- src/main.py
+++ src/main.py
@@ -4,50 +4,6 @@ import gzip
 import time
 from config.connect import connect
 import psycopg
-import csv
-
-# different way of copying authors
-# doesnt work because of the None spaces, which doesnt get separated :(
-def import_authors2(conn: psycopg.Connection) -> set:
-    """ Imports Twitter accounts from `authors.jsonl.gz` into `authors` table """
-
-    start_time = time.time()
-    inserted_authors = set()
-    cur = conn.cursor()
-    duplicate_rows = 0
-    with gzip.open('./tweets/authors.jsonl.gz') as file:
-        with cur.copy(
-            "COPY authors (id, name, username, description, followers_count, following_count, tweet_count, listed_count) FROM stdin (DELIMITER ',');"
-        ) as copy:
-            query = ''
-            for line in file:
-                author = json.loads(line)
-
-                if author['id'] in inserted_authors:
-                    duplicate_rows += 1
-                    continue
-                
-                row = (author['id'], author.get('name').replace("\x00", ""), author.get('username').replace("\x00", ""),
-                    author.get('description').replace("\x00", ""), author.get('public_metrics').get('followers_count'), 
-                    author.get('public_metrics').get('following_count'), author.get('public_metrics').get('tweet_count'), 
-                    author.get('public_metrics').get('listed_count'))
-                query += ','.join(str(val) for val in row) + '\n'
-
-                inserted_authors.add(author['id'])
-
-                if len(inserted_authors)%100000 == 0: 
-                    copy.write(query)
-                    query = ''
-                    print(f'Execution after {len(inserted_authors)} rows: {round(time.time() - start_time, 3)}s')
-
-        conn.commit()
-        cur.close()
-
-    print(f'Total execution time: {round(time.time() - start_time, 3)}s')
-    print(f'Total number of inserted rows: {len(inserted_authors)}')
-    print(f'Total number of duplicate rows: {duplicate_rows}')
-
-    return inserted_authors
 
 
 def import_authors(conn: psycopg.Connection) -> set:
@@ -96,8 +52,9 @@ def import_authors(conn: psycopg.Connection) -> set:
     return inserted_authors
 
 
-def import_links(conn: psycopg.Connection, convo_id: int, links: list) -> None:
-    cur = conn.cursor()
+def import_links(cur: psycopg.Cursor, convo_id: int, links: list) -> None:
+    """ Imports links from the `urls` array in the `entities` object """
+
     with cur.copy("COPY links (conversation_id, url, title, description) FROM stdin;") as copy:
         for link in links:
             if len(link['expanded_url']) > 2048: continue
@@ -140,7 +97,7 @@ def import_conversations(conn: psycopg.Connection, authors_ids: set) -> None:
 
                 if conversation.get('entities') != None:
                     if conversation.get('entities').get('urls') != None:
-                        import_links(conn, conversation['id'], conversation['entities']['urls'])
+                        import_links(cur, conversation['id'], conversation['entities']['urls'])
 
                 if len(inserted_convos)%10000 == 0: 
                     print(f'Execution after {len(inserted_convos)} rows: {round(time.time() - start_time, 3)}s')
